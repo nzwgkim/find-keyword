46fe4bd4d75354d1bd83e8e5e11b6dbadf440f53
Woody <nz.wgkim@gmail.com>
2022-10-01 02:01:19 +1300

    별코딩 React Hooks에 취한다 - Custom Hooks 커스텀 훅

--- src/App.js
+++ src/App.js
@@ -1,46 +1,34 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
+const baseUrl = 'https://jsonplaceholder.typicode.com';
 
 function App() {
-  const [word, setWord] = useState([]);
-  const [input, setInput] = useState('');
+  const [data, setData] = useState(null);
 
-  const kopoUrl = 'https://www.nzkoreapost.com/bbs/board.php?bo_table=market_buynsell&sca=&sop=and&sfl=wr_subject%7C%7Cwr_content&stx=';
-
-  //let url = kopoUrl + input;
-  let url = kopoUrl + 'TV';
-  url = 'https://jsonplaceholder.typicode.com/users';
-  console.log(url);
-
-  useEffect(()=>{
-    fetch('url')
-    .then(res=>res.json())
-    .then(res=>console.log(res));
-  },[]);
-
-  /*
-  fetch('url')
-  .then((response)=>(response.json()))
-  .then((data)=>console.log(data))
-  .catch((error) => console.log("error:", error));
-*/
+  const fetchUrl = (type) => {
+    fetch(baseUrl + '/' + type)
+      .then((res) => res.json())
+      .then((res) => setData(res))
+  };
 
 
+  console.clear();
 
-  const clickWord = ()=>{
-    setWord((prev)=>[input,...prev]);
-  }
-  const handleInputChange = (e) =>{
-    setInput(e.target.value);
-  }
-  console.log(word);
+  useEffect(() => {
+    fetchUrl('users');
+  }, []);
+
+  console.log(data);
+
+
   return (
-    <div className="App">
-
-      <input type='text' value={input} onChange={handleInputChange}></input>
-      <button onClick={clickWord}> Update </button>
-      <p>{word.map((w,id)=><p key={id}>{w}</p>)}</p>
+    <div>
+      <h1>useFetch</h1>
+      <button onClick={()=>fetchUrl('users')}>Users</button>
+      <button onClick={()=>fetchUrl('posts')}>Posts</button>
+      <button onClick={()=>fetchUrl('todos')}>Todos</button>
+      <pre>{JSON.stringify(data,null,2)}</pre>
     </div>
   );
 }
