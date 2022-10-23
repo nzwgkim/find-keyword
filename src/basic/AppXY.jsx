902188ec8c4e140f3e37c3f9d57081decb0a1da0
Woody <nz.wgkim@gmail.com>
2022-10-23 18:53:29 +1300

    mouse pointer

---
+++ src/basic/AppXY.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './AppXY.css'
+
+function AppXY() {
+    const [pos, setPos] = useState({ x: 0, y: 0})
+    return (
+        <div
+            className='container'
+            onPointerMove={ev => {
+                // setPos({ x: ev.clientX, y: ev.clientY })
+                setPos(prev=>({ ...prev, x: ev.clientX }))
+            }}>
+            <div
+                className='pointer'
+                style={{ transform: `translate(${pos.x}px, ${pos.y}px)` }}
+            />
+        </div>
+    )
+}
+
+export default AppXY
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppXY from './basic/AppXY';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import AppCounter from './AppCounter';
-import AppProducts from './AppProducts';
+// import AppProducts from './AppProducts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AppProducts />
+    <AppXY />
 );
 
 // If you want to start measuring performance in your app, pass a function
